b01cdd2374fd46849d6ae954520630c8f3186b35
Bilal SCOURI <bilalscouri@bilal-scouri-ccm-m2.pusq.etud.u-picardie.fr>
2023-11-14 15:00:09 +0100

    Fin du TP

--- main.tf
+++ main.tf
@@ -101,8 +101,69 @@ resource "aws_instance" "nat-instance" {
   ami           = data.aws_ami.amazon-ami.id
   instance_type = "t2.micro"
   source_dest_check = false
+  key_name = aws_key_pair.aws-key.key_name
+  vpc_security_group_ids = [aws_security_group.sg.id]
+  subnet_id = aws_subnet.subnet_public[each.key].id
 
   tags = {
     Name = "${var.vpc_name}-nat-${var.aws_region}${each.key}"
   }
+}
+
+resource "aws_instance" "instance-private" {
+  for_each = var.azs
+  ami           = data.aws_ami.amazon-ami.id
+  instance_type = "t2.micro"
+  key_name = aws_key_pair.aws-key.key_name
+  vpc_security_group_ids = [aws_security_group.sg.id]
+  subnet_id = aws_subnet.subnet_private[each.key].id
+
+  tags = {
+    Name = "${var.vpc_name}-nat-private-${var.aws_region}${each.key}"
+  }
+}
+
+resource "aws_eip" "eip-public" {
+  for_each = var.azs
+  domain = "vpc"
+}
+
+resource "aws_eip_association" "eip-public-association" {
+  for_each = var.azs
+  instance_id = aws_instance.nat-instance[each.key].id
+  allocation_id = aws_eip.eip-public[each.key].id
+}
+
+resource "aws_route_table" "rt-public" {
+  vpc_id = aws_vpc.vpc.id
+}
+
+resource "aws_route_table" "rt-private" {
+  for_each = var.azs
+  vpc_id = aws_vpc.vpc.id
+}
+
+resource "aws_route" "r-public" {
+  route_table_id            = aws_route_table.rt-public.id
+  gateway_id                = aws_internet_gateway.gw.id
+  destination_cidr_block    = "0.0.0.0/0"
+}
+
+resource "aws_route" "r-private" {
+  for_each = var.azs
+  route_table_id            = aws_route_table.rt-private[each.key].id
+  network_interface_id      = aws_instance.nat-instance[each.key].primary_network_interface_id
+  destination_cidr_block    = "0.0.0.0/0"
+}
+
+resource "aws_route_table_association" "a" {
+  for_each = var.azs
+  subnet_id      = aws_subnet.subnet_public[each.key].id
+  route_table_id = aws_route_table.rt-public.id
+}
+
+resource "aws_route_table_association" "a-private" {
+  for_each = var.azs
+  subnet_id      = aws_subnet.subnet_private[each.key].id
+  route_table_id = aws_route_table.rt-private[each.key].id
 }
